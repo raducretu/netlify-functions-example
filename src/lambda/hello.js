2cda5889313c2e79a2e5cadf5956fdcc65d08b4f
Radu Cretu <ameriza@gmail.com>
2018-07-17 02:31:11 +0300

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -4,6 +4,11 @@ exports.handler = function(event, context, callback) {
   console.log(event);
   callback(null, {
     statusCode: 200,
-    body: 'test'
+    body: 'test',
+      "headers": {
+      "X-Test-Header": "baking experiment",
+      "Set-Cookie": "cookie1=chocolate-chip",
+      "Set-Cookie": "cookie2=oatmeal"
+    }
   });
 };
