a10213327ba5fd1613a06a6d9473456571a2b422
Radu Cretu <ameriza@gmail.com>
2018-07-17 01:59:05 +0300

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,6 +1,7 @@
 exports.handler = function(event, context, callback) {
   console.log('test');
   console.log(context);
+  console.log(event);
   callback(null, {
     statusCode: 200,
     body: 'test'
